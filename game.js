023832626303b634955e345b1f930ac890429b50
Jacob Hunter <jacobhunter255@gmail.com>
2024-01-25 19:27:56 +0800

    boilerplate

---
+++ game.js
@@ -0,0 +1,17 @@
+
+//first step - create the game board. 16 x 16 grid of square divs.
+let width = 16
+let height = 16
+
+for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+
+    }
+}
+
+
+//second step - format a flexbox to put the grid in
+
+//third step - set up "hover" effect so divs change colour when mouse passes over
+
+//fouth step - create button to allow new grid to be created. see button and prompts
