c05192b52e2ecbc629caddb66e614ce820a9cd05
Jacob Hunter <jacobhunter255@gmail.com>
2024-02-10 09:37:04 +0800

    improve functionality of game code

--- game.js
+++ game.js
@@ -3,8 +3,14 @@ console.log("Game initialised.");
 let width = 16
 let height = 16
 let dots = 256
+const dotSize = 10
+const padding = 5;
 
 const gameContainer = document.getElementById("game-container");
+//resize game container depending on number of dots to play with.
+//calculation is based on number of dots wide and a padded amount around each dot.
+gameContainer.style.width = width * (dotSize + padding) + padding;
+
 
 
 
